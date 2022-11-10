d8cdca8903540fd0b3c07df5bc259e9d865c4113
David Perelman <dperelman3@gmail.com>
2022-11-10 19:45:31 +0200

    setup our first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   // 1) Rendering the component we want to test
+//   render(<App />);
+
+//   // 2) Finding the elements
+//   const linkElement = screen.getByText(/learn react/i);
+
+//   // 3) Assertion
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('inputs should be initally empty', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const emailInputElement = screen.getByRole('textbox');
+  expect(emailInputElement.value).toBe('');
 });
