d98866a8bf8cd03f331716fc9fd0a15d68723fc6
David Perelman <dperelman3@gmail.com>
2022-11-10 20:00:17 +0200

    setup password input test

--- src/App.test.js
+++ src/App.test.js
@@ -15,5 +15,7 @@ import App from './App';
 test('inputs should be initally empty', () => {
   render(<App />);
   const emailInputElement = screen.getByRole('textbox');
+  const passwordInputElement = screen.getByLabelText(/password/i);
   expect(emailInputElement.value).toBe('');
+  expect(passwordInputElement.value).toBe('');
 });
